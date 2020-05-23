b09571b82234915153dade11193042ba279237b7
Mišo Belica <miso.belica@gmail.com>
2020-05-23 18:58:41 +0200

    Execute tasks with Invoke and use Fabric as library
    I think the Fabric should be designed like this from the
    beginning. It does not make sense to make CLI app from the
    SSH library. Invoke is OK and we can use Fabric there if
    needed.

--- tasks.py
+++ tasks.py
@@ -7,9 +7,9 @@ Setup:
 """
 from pathlib import Path
 
-from fabric import task as remote_task
+from fabric import Connection
 from invocations.console import confirm
-from invoke import run as run_local, task as local_task
+from invoke import task
 
 DEFAULT_PASSWORD_LENGTH = 25
 DOCKER_AVAILABLE_COMMANDS = {"ps", "term", "stop", "start", "restart", "rm", "logs"}
@@ -20,42 +20,45 @@ DOCKER_COMMANDS = {
 }
 
 
-@remote_task
-def server_init(c):
-    c.put("dotfiles/.bash_aliases", "~/.bash_aliases")
-    c.put("dotfiles/.gitconfig", "~/.gitconfig")
-    c.put("dotfiles/.vimrc", "~/.vimrc")
-    c.put("dotfiles/.bashrc", "~/bashrc.append")
-    c.run("cat ~/.bashrc ~/bashrc.append > ~/bashrc.new")
-    c.run("mv ~/.bashrc ~/bashrc.backup")
-    c.run("mv ~/bashrc.new ~/.bashrc")
-    c.run("rm -f ~/bashrc.append ~/bashrc.new")
+@task
+def server_init(_, host, user=None):
+    """<host> [--user=<name>]"""
+    with Connection(host, user) as remote:
+        remote.put("dotfiles/.bash_aliases", "~/.bash_aliases")
+        remote.put("dotfiles/.gitconfig", "~/.gitconfig")
+        remote.put("dotfiles/.vimrc", "~/.vimrc")
+        remote.put("dotfiles/.bashrc", "~/bashrc.append")
+        remote.run("cat ~/.bashrc ~/bashrc.append > ~/bashrc.new")
+        remote.run("mv ~/.bashrc ~/bashrc.backup")
+        remote.run("mv ~/bashrc.new ~/.bashrc")
+        remote.run("rm -f ~/bashrc.append ~/bashrc.new")
 
 
-@local_task
+@task
 def password(c, nginx=False, length=DEFAULT_PASSWORD_LENGTH, special_chars=True):
     """[--nginx --length=25 --no-special-chars]"""
     print(generate_password(length, special_chars))
     if nginx:
-        run_local("openssl passwd -apr1")
+        c.run("openssl passwd -apr1")
 
 
-@remote_task
-def add_group(c, group="docker"):
-    """[--group=docker]"""
-    c.run(f"usermod -a -G {group} {c.user}")
+@task
+def add_group(_, host, user, group="docker"):
+    """<host> [--group=docker]"""
+    with Connection(host, user) as remote:
+        remote.run(f"usermod -a -G {group} {user}")
 
 
-@remote_task(password)
-def create_user(c, name, creator="root"):
-    """<name> [--creator=<root>]"""
-    c.user = creator
+@task
+def create_user(c, host, name, creator="root"):
+    """<host> <name> [--creator=<root>]"""
+    password(c)
+    with Connection(host, creator) as remote:
+        remote.sudo(f"useradd --groups docker --create-home --shell /bin/bash {name}")
+        remote.sudo(f"passwd {name}")
 
-    c.sudo(f"useradd --groups docker --create-home --shell /bin/bash {name}")
-    c.sudo(f"passwd {name}")
 
-
-@local_task
+@task
 def etc_hosts(_, add=None, remove=None):
     """[--add="127.0.0.1 postgres" | --remove=<ip/host>] """
     path = Path("c:/Windows/System32/drivers/etc/hosts")
@@ -65,7 +68,7 @@ def etc_hosts(_, add=None, remove=None):
     if add:
         new_lines = content.splitlines() + [add]
     elif remove:
-        new_lines = [l for l in content.splitlines() if not l.startswith(remove) and not l.endswith(remove)]
+        new_lines = [line for line in content.splitlines() if not line.startswith(remove) and not line.endswith(remove)]
 
     if new_lines:
         new_content = "\n".join(new_lines)
@@ -77,7 +80,7 @@ def etc_hosts(_, add=None, remove=None):
         print(content)
 
 
-@local_task
+@task
 def ssh_key(c, comment):
     """--comment=<e-mail>"""
     c.run(f'ssh-keygen -t rsa -b 4096 -C "{comment}"')
@@ -92,10 +95,10 @@ def generate_password(length, special_chars):
     if special_chars:
         chars += "|?!@#$%^&*(),;~"
 
-    return ''.join(generator.choice(chars) for i in range(length))
+    return ''.join(generator.choice(chars) for _ in range(length))
 
 
-@local_task
+@task
 def pg_dump(c, database, only_schema=False):
     """--database=<name> [--only-schema]"""
     c.run(
@@ -110,7 +113,7 @@ def pg_dump(c, database, only_schema=False):
         )
 
 
-@local_task
+@task
 def pg_load(c, database, only_data=True):
     """--database=<name> [--only-data]"""
     if not only_data:
@@ -119,37 +122,38 @@ def pg_load(c, database, only_data=True):
     c.run(f'psql -U postgres -d "{database}" -f data.sql')
 
 
-@remote_task(iterable=["container"])
-def docker(c, statement, container):
-    """-H example.com,domain.org ps | (logs | term | stop | start | restart | rm --container=<name>...)"""
+@task(iterable=["container"])
+def docker(_, host, statement, container):
+    """<host> ps | (logs | term | stop | start | restart | rm --container=<name>...)"""
     if statement not in DOCKER_AVAILABLE_COMMANDS:
         print(f"Please specify valid docker statement: {DOCKER_AVAILABLE_COMMANDS}")
         return
 
-    if statement == "ps":
-        c.run("docker " + DOCKER_COMMANDS.get(statement, statement))
-    else:
-        for name in container:
-            c.run(f"docker {DOCKER_COMMANDS.get(statement, statement)} {name}", warn=True)
+    with Connection(host) as remote:
+        if statement == "ps":
+            remote.run("docker " + DOCKER_COMMANDS.get(statement, statement))
+        else:
+            for name in container:
+                remote.run(f"docker {DOCKER_COMMANDS.get(statement, statement)} {name}", warn=True)
 
 
-@local_task
+@task
 def git_changes(c, base_branch="master"):
     """[--patterns=<file>]"""
-    result = run_local("git branch --all", hide="out")
-    branches = [l.strip(" *") for l in result.stdout.splitlines() if "->" not in l]
+    result = c.run("git branch --all", hide="out")
+    branches = [line.strip(" *") for line in result.stdout.splitlines() if "->" not in line]
 
     changed_files = set()
     for branch in branches:
         if branch != base_branch and not branch.startswith("remotes/"):
-            result = run_local(f"git merge-base {branch} {base_branch}", hide="out", echo=False, warn=True)
-            result = run_local(f"git diff --name-only {branch} {result.stdout}", hide="out", echo=False, warn=True)
+            result = c.run(f"git merge-base {branch} {base_branch}", hide="out", echo=False, warn=True)
+            result = c.run(f"git diff --name-only {branch} {result.stdout}", hide="out", echo=False, warn=True)
             changed_files.update(result.stdout.splitlines())
 
     print("\n".join(sorted(changed_files)))
 
 
-@local_task
+@task
 def python_profile(c, command):
     """script.py
 
@@ -158,5 +162,4 @@ def python_profile(c, command):
     pip install pyprof2calltree
     pyprof2calltree -i profiling.out -k
     """
-    run_local(f"python -m cProfile -s cumtime -o profiling.out {command}")
-
+    c.run(f"python -m cProfile -s cumtime -o profiling.out {command}")
